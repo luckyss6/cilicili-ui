633496711e198aed17718a7b76a5b9639bdfdbc6
luckyss6 <1170847775@qq.com>
2024-06-12 17:24:45 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import {BrowserRouter} from "react-router-dom";
+import { HashRouter} from "react-router-dom";
 import AppRoutes from "./routes/index.jsx";
 
 export default function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <AppRoutes />
-        </BrowserRouter>
+        </HashRouter>
     )
 }
